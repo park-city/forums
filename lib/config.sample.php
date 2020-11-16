9ada423b0808c8fc771ab22856a0c10acc4a8bc8
kaj <57120895+tierage@users.noreply.github.com>
2020-11-15 21:29:41 -0500

    Update config.sample.php

--- lib/config.sample.php
+++ lib/config.sample.php
@@ -11,14 +11,4 @@ $dbpref = '';
 
 $salt = "";
 
-define('DEFAULT_THEME', 'parkcity');
-
-// Power levels, avoid touching
-define('BANNED_GROUP', -1);
-define('DEFAULT_GROUP', 0);
-define('LMOD_GROUP', 1);
-define('MOD_GROUP', 2);
-define('ADMIN_GROUP', 3);
-define('ROOT_GROUP', 4);
-
 ?>
